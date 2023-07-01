8701ebfe4d25a94b126f35b634e14aebf54eca9e
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-01 12:24:15 +0800

    Display data API without loading

--- public/app.js
+++ public/app.js
@@ -7,22 +7,19 @@
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 function App() {
+  const [news, setNews] = React.useState([]);
   React.useEffect(function () {
-    // const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-    //                 .then(function(response) {
-    //                     return response.json();
-    //                 })
-    //                 .then(function(response){
-    //                     return response
-    //                 })
-
     async function getData() {
       const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
       const response = await request.json();
-      console.log(response);
+      setNews(response);
     }
     getData();
   }, []);
-  return /*#__PURE__*/React.createElement("h1", null, "Data Fetch");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), /*#__PURE__*/React.createElement("ul", null, news.map(function (item) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id
+    }, item.title);
+  })));
 }
 root.render( /*#__PURE__*/React.createElement(App, null));
--- src/app.js
+++ src/app.js
@@ -8,26 +8,28 @@ const container = document.querySelector('#root')
 const root = ReactDOM.createRoot(container)
 
 function App(){
+    const [news, setNews] = React.useState([])
     React.useEffect(function(){
-        // const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-        //                 .then(function(response) {
-        //                     return response.json();
-        //                 })
-        //                 .then(function(response){
-        //                     return response
-        //                 })
-
         async function getData(){
             const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs')
             
             const response = await request.json();
 
-            console.log(response)
+            setNews(response)
         }
-
+        
         getData();
     }, [])
-    return <h1>Data Fetch</h1>
+    return (
+    <>
+        <h1>Data Fetch</h1>
+        <ul>
+            {news.map(function(item){
+                return <li key={item.id}>{item.title}</li>
+            })}
+        </ul>
+    </>
+    )
 }
 
 root.render(<App />)
