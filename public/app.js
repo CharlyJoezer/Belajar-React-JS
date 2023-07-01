6b7ba030a8db4400d90a10d8af32cd266f90cc12
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-01 20:47:07 +0800

    Aplikasi To do list 4

--- public/app.js
+++ public/app.js
@@ -9,14 +9,27 @@ const root = ReactDOM.createRoot(container);
 function App() {
   const [activity, setActivity] = React.useState('');
   const [todos, setTodos] = React.useState([]);
+  const [edit, setEdit] = React.useState({});
   function generateId() {
     return Date.now();
   }
-  function eventTodoHandler(event) {
+  function saveTodoHandler(event) {
     event.preventDefault();
+    if (edit.id) {
+      const editTodoIndex = todos.findIndex(function (todo) {
+        return todo.id == edit.id;
+      });
+      const updatedTodo = {
+        id: edit.id,
+        activity: activity
+      };
+      const updatedTodos = [...todos];
+      updatedTodos[editTodoIndex] = updatedTodo;
+      return setTodos(updatedTodos);
+    }
     setTodos([...todos, {
       id: generateId(),
-      activity: activity
+      activity
     }]);
     setActivity('');
   }
@@ -26,8 +39,12 @@ function App() {
     });
     setTodos(filterTodos);
   }
+  function editTodoHandler(todo) {
+    setActivity(todo.activity);
+    setEdit(todo);
+  }
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple Todo List"), /*#__PURE__*/React.createElement("form", {
-    onSubmit: eventTodoHandler
+    onSubmit: saveTodoHandler
   }, /*#__PURE__*/React.createElement("input", {
     type: "text",
     value: activity,
@@ -35,10 +52,12 @@ function App() {
     onChange: function (event) {
       setActivity(event.target.value);
     }
-  }), /*#__PURE__*/React.createElement("button", null, "Tambah")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
+  }), /*#__PURE__*/React.createElement("button", null, edit.id ? 'Simpan Perubahan' : 'Simpan')), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
       key: todo.id
     }, todo.activity, /*#__PURE__*/React.createElement("button", {
+      onClick: editTodoHandler.bind(this, todo)
+    }, "Edit"), /*#__PURE__*/React.createElement("button", {
       onClick: removeTodoHandler.bind(this, todo.id)
     }, "Hapus"));
   })));
--- src/app.js
+++ src/app.js
@@ -10,17 +10,31 @@ const root = ReactDOM.createRoot(container)
 function App(){
     const [activity, setActivity] = React.useState('')
     const [todos, setTodos] = React.useState([])
+    const [edit, setEdit] = React.useState({})
 
     function generateId(){
         return Date.now()
     }
 
-    function eventTodoHandler(event){
+    function saveTodoHandler(event){
         event.preventDefault()
+        if(edit.id){
+            const editTodoIndex = todos.findIndex(function(todo){
+                return todo.id == edit.id
+            });
+            const updatedTodo = {
+                id : edit.id,
+                activity : activity
+            }
+            const updatedTodos = [...todos]
+            updatedTodos[editTodoIndex] = updatedTodo
+            return setTodos(updatedTodos)
+
+        }
 
         setTodos([... todos, {
             id : generateId(),
-            activity : activity
+            activity
         }])
         setActivity('')
     }
@@ -32,18 +46,27 @@ function App(){
 
         setTodos(filterTodos)
     }
+
+    function editTodoHandler(todo){
+        setActivity(todo.activity)
+        setEdit(todo)
+    }
+
     return (
         <>
             <h1>Simple Todo List</h1>
-            <form onSubmit={eventTodoHandler}>
+            <form onSubmit={saveTodoHandler}>
                 <input type="text" value={activity} placholder="Aktifitas" onChange={function(event){
                     setActivity(event.target.value);
                 }}/>
-                <button>Tambah</button>
+                <button>
+                    {edit.id ? 'Simpan Perubahan' : 'Simpan'}
+                </button>
             </form>
             <ul>
                 {todos.map(function(todo){
                     return <li key={todo.id}>{todo.activity}
+                            <button onClick={editTodoHandler.bind(this, todo)}>Edit</button>
                             <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
                            </li>
                 })}
